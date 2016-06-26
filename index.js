9eb7f4c2cd8cf8a5dbabed99d0b65c62b44234a3
vagrant <vagrant@homestead>
2016-06-26 10:36:32 +0000

    new tutsplus chat

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require("express");
+var app = express();
+var port = 3700;
 
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-io.on('connection', function(socket){
- socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+app.get("/", function(req, res){
+    res.send("It works!");
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+app.listen(port);
+console.log("Listening on port " + port);
